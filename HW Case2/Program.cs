bdcc484bee29ef98d9961bbe67cc5d2157799713
Margarita <bano4kaa@mail.ru>
2022-08-20 15:37:14 +0300

    full cases

--- HW Case2/Program.cs
+++ HW Case2/Program.cs
@@ -1,2 +1,18 @@
-﻿    // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y)
+Console.WriteLine("max = " + x);
+if (x < y)
+Console.WriteLine("max = " + y);  
+if (x <  y)
+Console.WriteLine("min = " + x);
+if (x > y)
+Console.WriteLine("min = " + y);
---
+++ HW Case4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int z = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (x > max)
+max = x;
+if (y > max)
+max = y;
+if (z > max)
+max = z;
+Console.WriteLine(max);
